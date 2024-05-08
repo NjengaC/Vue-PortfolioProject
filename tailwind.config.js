0bdb0bc6cdec8fbd836d68182b1ad3e082dd8c9c
kaymacharia <kaymachariaa@gmail.com>
2024-05-08 14:47:15 +0300

    kay

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Vue-PortfolioProject/entry/templates/**/*.html", "./Vue-PortfolioProject/entry/static/styles/**/*.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
